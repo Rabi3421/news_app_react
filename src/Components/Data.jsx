2f5619255b6aa54f8c3f68609fe6709092148049
Rabi3421 <rabinarayan3421@gmail.com>
2022-08-20 18:17:31 +0530

    initialise

---
+++ src/Components/Data.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+
+const Data = () => {
+  let isLoading = true;
+  
+  
+  let api = 'http://hn.algolia.com/api/v1/search?query=...';
+    const fetchApiData = async(url) =>{
+      try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data.hits);
+        // isLoading = false;
+      }catch(error){
+        console.log(error);
+      }
+    };
+    
+    useEffect(()=>{
+      fetchApiData(api);
+    },[]);
+    if(isLoading){
+      return(
+      <>
+      <h3>Loading ...</h3>
+      </>
+    )}
+
+    return (
+    <>
+    <h2>my tech news post</h2>
+    </>
+  )
+}
+
+export default Data
